8b4676e521d096d989e18f2a817f9fb01fd4c395
entl <maxvorobyov01@gmail.com>
2025-03-19 18:48:35 +0000

    feat: configure tailwind and shadcn

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-<!--    <NuxtWelcome />-->
-    <h1 class="bg-red-500 text-red-600">hui</h1>
+    <Toaster></Toaster>
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,77 @@
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { resolve } from "path";
+//
+// export default defineNuxtConfig({
+//   compatibilityDate: '2024-11-01',
+//   devtools: { enabled: true },
+//   css: ["~/assets/css/tailwind.css"],
+//
+//   postcss: {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     },
+//   },
+//
+//   modules: ["shadcn-nuxt"],
+//
+//   shadcn: {
+//     /**
+//      * Prefix for all the imported component
+//      */
+//     prefix: '',
+//     /**
+//      * Directory that the component lives in.
+//      * @default "./components/ui"
+//      */
+//     componentDir: './components/ui'
+//   }
+// })
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from "path";
+import {resolve} from "path";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  alias: {
-    "@": resolve(__dirname, "src"),
-  },
-  css: ["~/assets/css/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    compatibilityDate: '2024-04-03',
+    devtools: {
+        enabled: true,
+
+        timeline: {
+            enabled: true
+        }
     },
-  }
+    modules: [
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        '@nuxtjs/color-mode',
+        '@nuxt/icon',
+        '@nuxtjs/leaflet'
+    ],
+    leaflet: {
+        markerCluster: true
+    },
+    icon: {
+        customCollections: [
+            {
+                prefix: 'my-icon',
+                dir: './assets/icons'
+            }
+        ]
+    },
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    },
+    runtimeConfig: {
+        public: {
+            apiBase: process.env.NUXT_BASE_API_URL,
+        }
+    }
 })
