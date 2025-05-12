e59ba5d3743c7c9d4c712e840c97c4fe942014c3
entl <maxvorobyov01@gmail.com>
2025-05-12 23:29:54 +0100

    feat: add layout for auth pages with sidebar

---
+++ layouts/authorised.vue
@@ -0,0 +1,11 @@
+<!-- layout.vue -->
+<template>
+  <SidebarProvider>
+    <div class="flex w-full min-h-screen">
+      <AppSidebar />
+      <div class="flex-1 min-w-0">
+        <slot />
+      </div>
+    </div>
+  </SidebarProvider>
+</template>
